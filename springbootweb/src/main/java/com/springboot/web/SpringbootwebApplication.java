9fed9947c42fe170e96c5cd22f52addf15782008
Bivek Jha <biveknitd76@gmail.com>
2020-12-20 08:57:53 +0530

    commit spring boot web

---
+++ springbootweb/src/main/java/com/springboot/web/SpringbootwebApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+	
+	@SpringBootApplication
+	public class SpringbootwebApplication extends SpringBootServletInitializer{
+	    public static void main(String[] args) {
+	        SpringApplication.run(SpringbootwebApplication.class, args);
+	    }
+	}
+
